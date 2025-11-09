0a9fffa4cc80acb30a86baeffb8174f6ec4e6a70
Luis <alphageekdom@gmail.com>
2025-11-09 11:15:43 -0800

    limit and sort ideas from the API;2D

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -20,11 +20,7 @@ export const Route = createFileRoute('/ideas/')({
 });
 
 function IdeasPage() {
-  const { data } = useSuspenseQuery(ideasQueryOptions());
-  const ideas = [...data].sort(
-    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-  );
-  // console.log(ideas);
+  const { data: ideas } = useSuspenseQuery(ideasQueryOptions());
 
   return (
     <div className="p-4">
