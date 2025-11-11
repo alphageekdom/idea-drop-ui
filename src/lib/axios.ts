2c7a04a1f32c7ba75a4e2d0b35dcec7ef803d286
Luis <alphageekdom@gmail.com>
2025-11-11 10:23:26 -0800

    prepare for deployment

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,8 @@ import { getStoredAccessToken, setStoredAccessToken } from './authToken';
 import { refreshAccessToken } from '@/api/auth';
 
 const api = axios.create({
-  baseURL: '/api',
+  // baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
