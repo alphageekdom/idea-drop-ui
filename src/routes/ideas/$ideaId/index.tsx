7312c471d81c91bcbb98f5d5331c2a5cb900d8a8
Luis <alphageekdom@gmail.com>
2025-11-10 17:31:22 -0800

    hider user controls

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -5,6 +5,7 @@ import {
   useMutation,
 } from '@tanstack/react-query';
 import { fetchIdea, deleteIdea } from '@/api/ideas';
+import { useAuth } from '@/context/AuthContext';
 
 const ideaQueryOptions = (ideaId: string) =>
   queryOptions({
@@ -24,6 +25,8 @@ function IdeaDetailsPage() {
   const { data: idea } = useSuspenseQuery(ideaQueryOptions(ideaId));
 
   const navigate = useNavigate();
+
+  const { user } = useAuth();
 
   const { mutateAsync: deleteMutate, isPending } = useMutation({
     mutationFn: () => deleteIdea(ideaId),
@@ -50,23 +53,28 @@ function IdeaDetailsPage() {
       <h2 className="text-2xl font-bold">{idea.title}</h2>
       <p className="mt-2">{idea.description}</p>
 
-      {/* Edit Link */}
-      <Link
-        to="/ideas/$ideaId/edit"
-        params={{ ideaId }}
-        className="mt-4 mr-2 inline-block rounded bg-yellow-500 px-4 py-2 text-sm text-white transition hover:bg-yellow-600"
-      >
-        Edit
-      </Link>
+      {/* User Controls */}
+      {user && user.id === idea.user && (
+        <>
+          {/* Edit Link */}
+          <Link
+            to="/ideas/$ideaId/edit"
+            params={{ ideaId }}
+            className="mt-4 mr-2 inline-block rounded bg-yellow-500 px-4 py-2 text-sm text-white transition hover:bg-yellow-600"
+          >
+            Edit
+          </Link>
 
-      {/* Delete Button */}
-      <button
-        className="disabled:opacity:50 mt-4 cursor-pointer rounded bg-red-600 px-4 py-2 text-sm text-white transition hover:bg-red-700"
-        disabled={isPending}
-        onClick={handleDelete}
-      >
-        {isPending ? 'Deleting...' : 'Delete'}
-      </button>
+          {/* Delete Button */}
+          <button
+            className="disabled:opacity:50 mt-4 cursor-pointer rounded bg-red-600 px-4 py-2 text-sm text-white transition hover:bg-red-700"
+            disabled={isPending}
+            onClick={handleDelete}
+          >
+            {isPending ? 'Deleting...' : 'Delete'}
+          </button>
+        </>
+      )}
     </div>
   );
 }
